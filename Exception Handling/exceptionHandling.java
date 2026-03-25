404c9686c87da98a498102a210102ced26c4c488
Harini1305 <165678461+Harini1305@users.noreply.github.com>
2026-03-25 17:27:28 +0530

    Add files via upload

---
+++ Exception Handling/exceptionHandling.java
@@ -0,0 +1,30 @@
+package examples;
+import java.util.Scanner;
+
+
+	class InvalidAgeException extends Exception {
+	    public InvalidAgeException(String message) {
+	        super(message);
+	    }
+	}
+
+	public class exceptionHandling {
+
+	    public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+
+	        try {
+	            System.out.print("Enter your age: ");
+	            int age = s.nextInt();
+
+	            if (age < 0 ) {
+	                throw new InvalidAgeException("Age must be  above 0");
+	            }
+
+	            System.out.println("Valid age");
+
+	        } catch (InvalidAgeException e) {
+	            System.out.println(e.getMessage());
+	        } 
+	    }
+	}
